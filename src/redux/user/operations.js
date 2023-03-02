9ec63b2c1127ce8b2b33310ac3de5649f591f4df
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-03-02 14:39:33 +0200

    Update operations.js

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -11,9 +11,9 @@ const setAuthHeader = token => {
 };
 
 // Utility to remove JWT
-const clearAuthHeader = () => {
-  instance.defaults.headers.common.Authorization = '';
-};
+// const clearAuthHeader = () => {
+//   instance.defaults.headers.common.Authorization = '';
+// };
 
 export const register = createAsyncThunk(
   'user/register',
